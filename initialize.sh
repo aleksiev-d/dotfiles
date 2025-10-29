73a75b670508ada1673453121ed0711293c83eb3
Danail Aleksiev <aleksiev_d@yahoo.com>
2025-10-29 09:44:58 +0200

    update ideavimrc and initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,25 +1,25 @@
 CONFIG_DIR=~/Documents/configurations
 # MacOs Vs_Code path
-# VSCODE_USER_DIR=~/Library/Application\ Support/Code/User
+VSCODE_USER_DIR="$HOME/Library/Application Support/Code/User"
 
 # Linux Vs_Code path
-VSCODE_USER_DIR=~/.config/Code/User
+# VSCODE_USER_DIR=~/.config/Code/User
 
 echo "Setting up VS Code keybindings..."
-rm $VSCODE_USER_DIR/keybindings.json
-ln -s $CONFIG_DIR/vscode/keybindings.json $VSCODE_USER_DIR/keybindings.json
+rm "$VSCODE_USER_DIR/keybindings.json"
+ln -s "$CONFIG_DIR/vscode/keybindings.json" "$VSCODE_USER_DIR/keybindings.json"
 
 echo "Setting up VS Code settings..."
-rm $VSCODE_USER_DIR/settings.json
-ln -s $CONFIG_DIR/vscode/settings.json $VSCODE_USER_DIR/settings.json
+rm "$VSCODE_USER_DIR/settings.json"
+ln -s "$CONFIG_DIR/vscode/settings.json" "$VSCODE_USER_DIR/settings.json"
 
 echo "Setting up zsh configuration..."
 rm ~/.zshrc
-ln -s $CONFIG_DIR/zshrc/.zshrc ~/.zshrc
+ln -s "$CONFIG_DIR/zshrc/.zshrc" ~/.zshrc
 
 echo "Setting up IdeaVim configuration..."
 rm ~/.ideavimrc
-ln -s $CONFIG_DIR/rider/.ideavimrc ~/.ideavimrc
+ln -s "$CONFIG_DIR/rider/.ideavimrc" ~/.ideavimrc
 
 echo "Installing VS Code extensions..."
 while IFS= read -r extension || [ -n "$extension" ]; do
